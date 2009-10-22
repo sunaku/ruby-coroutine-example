96302a6dfb68395f37b5038ff0c2bd5debb6834b
Suraj N. Kurapati <sunaku@gmail.com>
2009-10-22 15:59:15 -0700

    add the require() inside Ruby script which causes Ruby bug

--- hello.rb
+++ hello.rb
@@ -4,3 +4,9 @@ puts "~~~~> Doing relay from #{__FILE__}:#{__LINE__}"
 relay_from_ruby_to_main
 puts "~~~~> OMG, back from main!! :-)"
 
+##
+# the following line causes a
+# "[BUG] object allocation during
+# garbage collection phase" error
+#
+require 'date'  # <== ERROR!
