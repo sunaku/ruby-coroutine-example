c72708250ad0540cb99327fdd629e9b49bf389d3
Suraj N. Kurapati <sunaku@gmail.com>
2009-10-23 07:19:43 -0700

    use `script -c ./run.sh` to capture output

--- run.sh
+++ run.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
+# enable core dumping
+ulimit -c unlimited
+
 for dir in ~/.multiruby/install/*
 do
   ver=$(basename "$dir")
-  echo $ver
 
-  (
-    export PATH=$dir/bin:$PATH
+  echo
+  echo "###############################################################"
+  echo "# $( "$dir"/bin/ruby -v )"
+  echo "###############################################################"
+  echo
 
-    # enable core dumping
-    ulimit -c unlimited
+  export PATH=$dir/bin:$PATH
 
-    # compile and run test case
-    ruby -v extconf.rb && sed -i 's,-shared,,g' Makefile && make && ./main.so ||
+  # compile and run test case
+  ruby -v extconf.rb &&
+  sed -i 's,-shared,,g' Makefile &&
+  make && ./main.so ||
 
-    # inspect the core dump, if any
-    test -f core && echo "bt full" > $$ && gdb -batch -x $$ ./main.so core
+  # inspect the core dump, if any
+  test -f core &&
+  echo "bt full" > $$ &&
+  gdb -batch -x $$ ./main.so core < /dev/null
 
-    # clean up
-    rm -f $$
-    make distclean
-
-  ) >& "$ver.log"
+  # clean up
+  rm -f $$
+  make distclean
 done
 
